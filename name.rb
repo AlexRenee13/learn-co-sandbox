a719345350fba3a97b674fe293ea333ffb3092d2
Alexis Palacios <alexis1994662@yahoo.com>
2019-11-26 23:29:37 +0000

    Automatically backed up by Learn

---
+++ name.rb
@@ -0,0 +1,25 @@
+# name= "The Queen of Hearts"
+# if name == "Alice"
+#   puts "Hello, Alice"
+
+# elsif name== "The White Rabbit" 
+#   puts "Don't be late, White Rabbit" 
+# elsif name== "The Mad Hatter" 
+#   puts "Welcome to the tea party, Mad Hatter"
+# elsif name== "The Queen of Hearts" 
+#   puts "Please don't chop off my head!" 
+# else puts "Whooooo are you?" 
+#   end
+
+# nested hashes
+school={
+  instructors: ["Ian", "Johann", "Alex"],
+  student: ["Andrew", "Howard", "Terrance", "Daniel", "Rachel", "Natalie"]
+  classes: ["Software Engineering", "Data Science"]
+}
+  
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
---
+++ name_of_file.rb
@@ -0,0 +1,5 @@
+my_array= []
+my_array= Array.new 
+  puppies=["Bulldog", "terrier" , "poodle"]
+  random_number= [2 , 5 , 6, 8, 30050]
+  mixed= ["this", "array", 7, 20, "has", 45 "integers", "&", "strings". 309]
